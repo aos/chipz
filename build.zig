84e1be6cb6e121cd39df7ac4ac6127eec910f98b
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2025-02-11 22:32:15 -0800

    wip: get SDL working

--- build.zig
+++ build.zig
@@ -11,6 +11,15 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+        .preferred_link_mode = .static,
+    });
+    const sdl_lib = sdl_dep.artifact("SDL3");
+    // const sdl_test_lib = sdl_dep.artifact("SDL3_test");
+
+    exe.root_module.linkLibrary(sdl_lib);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,54 @@
 const std = @import("std");
 const Chip8 = @import("Chip8.zig");
 
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+});
+
 pub fn main() !void {
     var aa = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = aa.allocator();
     defer aa.deinit();
+
+    std.log.debug("SDL build time version: {d}.{d}.{d}", .{
+        c.SDL_MAJOR_VERSION,
+        c.SDL_MINOR_VERSION,
+        c.SDL_MICRO_VERSION,
+    });
+    std.log.debug("SDL build time revision: {s}", .{c.SDL_REVISION});
+    {
+        const version = c.SDL_GetVersion();
+        std.log.debug("SDL runtime version: {d}.{d}.{d}", .{
+            c.SDL_VERSIONNUM_MAJOR(version),
+            c.SDL_VERSIONNUM_MINOR(version),
+            c.SDL_VERSIONNUM_MICRO(version),
+        });
+        const revision: [*:0]const u8 = c.SDL_GetRevision();
+        std.log.debug("SDL runtime revision: {s}", .{revision});
+    }
+
+    c.SDL_SetMainReady();
+
+    try errify(c.SDL_SetAppMetadata("Speedbreaker", "0.0.0", "example.zig-examples.breakout"));
+
+    try errify(c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD));
+    defer c.SDL_Quit();
+
+    std.log.debug("SDL video drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentVideoDriver().?,
+        c.SDL_GetNumVideoDrivers(),
+        c.SDL_GetVideoDriver,
+    )});
+    std.log.debug("SDL audio drivers: {}", .{fmtSdlDrivers(
+        c.SDL_GetCurrentAudioDriver().?,
+        c.SDL_GetNumAudioDrivers(),
+        c.SDL_GetAudioDriver,
+    )});
 
     var it = try std.process.argsWithAllocator(allocator);
 
@@ -18,7 +62,36 @@ pub fn main() !void {
 
     // const c8 = Chip8.init(.{ .debug = true });
     // try c8.load(allocator, rom_path);
-    // c8.start();
+    // while (true) {
+    //     c8.step();
+    //     if (c8.draw_flag) {
+    //         // draw something using SDL
+    //         c8.draw_flag = false;
+    //     }
+    //     std.time.sleep(std.time.ns_per_s * 1);
+    // }
+}
+
+
+/// Converts the return value of an SDL function to an error union.
+inline fn errify(value: anytype) error{SdlError}!switch (@import("shims").typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@import("shims").typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
 }
 
 test {
